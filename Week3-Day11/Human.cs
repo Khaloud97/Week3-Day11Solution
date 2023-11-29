de0686d761eedeccaac6cb6ab5eec1b536bb6f69
Khaloud <hamedkhaloud71@gmail.com>
2023-11-29 13:47:29 +0400

    Add project files.

---
+++ Week3-Day11/Human.cs
@@ -0,0 +1,64 @@
+﻿
+
+namespace Week3_Day11
+{
+    internal class Human
+    {
+        public static int cnt;
+        private int id;
+        public string name;
+        public int age;
+        public string address;
+        private string email;
+       //public string email;
+        public Human()
+        {
+            
+        }
+
+        public Human(string name,int age, string address, string email)
+        {
+            //what hapend inside function stay inside function
+            cnt += 1;
+            this.name = name;   
+            this.age = ValidetAge(age) ? age : 0;// if age is true is number will retuern age but if Validet of this Age is false will return 0 
+            this.address = address;
+            this.email = email;
+            this.id = cnt;
+        }
+
+        //get function to id read only 
+        public int GetID(int id) {
+
+            return this.id;
+        }
+
+        //get function to read email  only 
+        public string GetEmail(string email)
+        {
+
+            return this.email;
+        }
+        //set function to do change i can add on it
+        public void SetEmail(string email)
+        {
+            this.email = email;
+        }
+        //Write get id as a body Expression Method
+       // public string GetEmail(string email)=> this.email;
+
+
+        // In this function i will chack what the user enter in the age input if write  0 or less And if >100 
+        public bool ValidetAge(int age)
+        {
+            if (age <= 0 || age >=100) {
+                return false;
+            
+            }
+            return true;
+        }
+
+        //body Expression Method
+        public string GreetingByName() => $"Hello {this.name} ,{this.age} ,from {this.address} , your email: {this.email}";
+    }
+}
---
+++ Week3-Day11/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading.Channels;
+
+namespace Week3_Day11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Humen System!");
+
+
+            Console.Write("Enter your Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: "); //what if user enter string or more than 100 or less than 0
+            string sage;
+            int.TryParse(Console.ReadLine(), out int age);//will chack the user value
+
+            Console.Write("Enter your Address: ");
+            string address = Console.ReadLine();
+
+            Console.Write("Enter your E-Mail: ");
+            string email = Console.ReadLine();
+
+            Human hum = new Human(name, age, address, email);
+            Console.WriteLine($"\nYour informaition: \n {hum.GreetingByName()} \n");
+            Console.WriteLine("------------------");
+
+
+
+            Human humen1 = new Human("Khaloud", 26, "Albatinah", "hamedKhaloud71@gmail.com");
+            Console.WriteLine(humen1.GreetingByName());
+            Console.WriteLine("------------------\n");
+
+            //read from constrautre write it  between()
+            Human humen2 = new Human(name: "Sara", age: 22, address: "Muscat", email: "Sara353@gmail.com");
+            Console.WriteLine(humen2.GreetingByName());
+
+            Console.WriteLine("------------------\n");
+            //read from object inslization write it  between{}
+            Human humen3 = new Human{ name = "Hamed", age = 50, address = "Albatinah"};
+            Console.WriteLine("-----------befor set and get");
+            Console.WriteLine(humen3.GreetingByName());
+
+
+            Console.WriteLine("\n----------After  email set and get:\n");
+            humen3.SetEmail("Hamed@gmail.com");//set for email becouse it was privet
+            Console.WriteLine(humen3.GetEmail(email));
+            Console.WriteLine(humen3.GreetingByName());
+
+            //-----------------------------------------------------------------------
+            Console.WriteLine("----------------------------------------------------------------------------");
+
+           Console.WriteLine("--------Encabsulatin:----------\n");
+            Human humen4 = new Human("Ail", 21, "Albatinah", "Ail876@gmail.com");
+            // Console.WriteLine(humen4.GetEmail());
+            Console.WriteLine("-------------befor set and get");
+            Console.WriteLine(humen4.GreetingByName());
+
+            Console.WriteLine("\n-----------After  email set and get:\n");
+            humen4.SetEmail("Ali@gmail.com");
+            Console.WriteLine(humen4.GetEmail(email));
+            Console.WriteLine(humen4.GreetingByName());
+
+        }
+    }
+}
